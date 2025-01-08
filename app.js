bc04fde3e04b951efe3ed86596961ede199274e7
int2itive <70378581+int2itive@users.noreply.github.com>
2025-01-08 16:59:41 +0000

    Header Navigation Animation

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const contentContainer = document.querySelector('.main--json-content');
 const triggers = document.querySelectorAll('.category-selections li a');
 const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#()?&//=]*)$/gi
+const main = document.querySelector('.ll-header');
 
 let links = document.querySelectorAll('.link--sections .link a');
+let hiddenState = "-hidden";
+let nav_dark = "page-header";
+
+var threshold = 30,
+    uBound = 7, 
+    position = 0, 
+    lastScroll = 0, 
+    n_event = 0;
 
 let jsonData = [
         {
@@ -83,4 +92,22 @@ links.forEach((link) => {
     // });
     jsonData.filter(entry => entry.tag === articleTag).map(fillContainer).join('');
   });
+});
+
+window.addEventListener("scroll", function () {
+  var position = window.scrollY || document.documentElement.scrollTop;
+
+  if (position > threshold && position > lastScroll) {
+    main.classList.add(hiddenState);
+  } else {
+    main.classList.remove(hiddenState);
+    if (window.scrollY > 40) {
+      main.classList.add(nav_dark);
+    } else {
+      main.classList.remove(nav_dark);
+    }
+  }
+
+  lastScroll = position <= 0 ? 0 : position;
+
 });
