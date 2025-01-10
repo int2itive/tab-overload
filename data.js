d4d83764c71a24c3f5903bc49e33e8e05e09ceed
int2itive <70378581+int2itive@users.noreply.github.com>
2025-01-10 10:24:45 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,94 @@
+let jsonData = [
+        {
+            "url": "https://www.freecodecamp.org/news/guide-to-javascript-promises/",
+            "title": "How Promises Work in JavaScript – A Comprehensive Guide",
+            "desc": "There are a few asynchronous features in JavaScript, and one of them is Promises.",
+            "imgUrl": "https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/cdn/1_kCDwU9HP6PHbo_joE0JKHQ_82qCSbjSa.png",
+            "tag": "Javascript"
+        },
+        {
+            "url": "https://zellwk.com/blog/callbacks/",
+            "title": "Callbacks in JavaScript",
+            "desc": "Have you come across the term “callback” but don’t know what it means? Don’t worry, you’re not alone. That’s what today’s article is for! You’ll learn what callbacks are, why they’re important, and how to use them.",
+            "imgUrl": "https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/cdn/bg_RMdr1Of2k.jpeg",
+            "tag": "Javascript"
+        },
+        {
+            "url": "https://ishadeed.com/article/clip-path/",
+            "title": "Understanding Clip Path in CSS",
+            "desc": "A clear explanation of how `clip-path` works in detail when to use it, and how you can use it today in your web development projects.",
+            "imgUrl": "https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/cdn/bg_RMdr1Of2k.jpeg",
+            "tag": "CSS"
+        },  
+        {
+            "url": "https://css-tricks.com/using-requestanimationframe/",
+            "title": "Using requestAnimationFrame",
+            "desc": "There used to be just one way to do a timed loop in JavaScript: setInterval(). The goal is sixty “frames” per second to appear smooth.",
+            "imgUrl": "https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/cdn/0_ge4CGKfh7VIqIgk7_WpbZXfAGY.png",
+            "tag": "Javascript"
+        },
+
+        {
+            "url": "https://medium.com/@Flowmapp/progressive-disclosure-10-great-examples-to-check-5e54c5e0b5b6",
+            "title": "Progressive Disclosure: Great Examples",
+            "desc": "When an interface is complex enough for users to understand it right, designers need to look for unique ways to present its functionality.",
+            "imgUrl": "https://ik.imagekit.io/ghow2otb3rc/Projects/The%20Work/c0v-members/alien-passion_d9lUYaQKa_yzSnkGEwki.jpg",
+            "tag": "UI/UX"
+        },
+        {
+            "url": "https://elijahmanor.com/blog/format-js-dates-and-times",
+            "title": "Natively Format JavaScript Dates and Times",
+            "desc": "It's interesting to note that native browser capabilities are pretty capable these days when formatting dates and times, and the browser support is quite impressive too!",
+            "imgUrl": "https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/cdn/bg_RMdr1Of2k.jpeg",
+            "tag": "Javascript"
+        },
+        {
+            "url": "https://dev.to/remrkabledev/nesting-template-literals-a-recommended-approach",
+            "title": "Nesting Template Literals: A Recommended Approach",
+            "desc": "Code quality is essential, as it impacts the overall software quality. In consequence, software quality affects the safety, security, and reliability of any codebase.",
+            "imgUrl": "https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/cdn/r0t-031c9650_oFvUux3-q_zOasSqqqf.jpg",
+            "tag": "Javascript"
+        },
+        {
+            "url": "https://wesbos.com/template-strings-html",
+            "title": "Creation of HTML with JavaScript’s Template Strings",
+            "desc": "Another feature of _template literals_ or template strings is the ability have multi-line strings without any funny business.",
+            "imgUrl": "https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/cdn/1_fX1vRVMR8F7jiHGzPyRv7Q_lY8mSsVMg.png",
+            "tag": "Javascript"
+        },
+        {
+            "url": "https://mastery.games/post/overlapping-grid-items/",
+            "title": "Overlapping Grid Items",
+            "desc": "Now that CSS Grid has arrived, we're starting to see more and more magazine style layouts on the web. And for good reason.",
+            "imgUrl": "https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/Artists/cbc029ab57a8495195bf6b4061e2cb2f_Qgk527FNL.jpg",
+            "tag": "Grid"
+        },
+        {
+            "url": "https://elijahmanor.com/blog/reducing-filter-and-map-down-to-reduce",
+            "title": "Reducing Filter and Map Down to Reduce",
+            "desc": "You've probably seen some of the new array methods in ECMAScript 5 (ES5) and you may have even used some of them before.",
+            "imgUrl": "https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/cdn/0_YmsNE0ZfkcrF6TvE_D9kltAo_e.png",
+            "tag": "Javascript"
+        },
+        {
+            "url": "https://css-tricks.com/the-sass-ampersand/",
+            "title": "The Sass Ampersand",
+            "desc": "The & is an extremely useful feature in Sass (and Less). It’s used when nesting. It can be a nice time-saver when you know how to use it.",
+            "imgUrl": "https://ik.imagekit.io/ghow2otb3rc/Projects/The%20Work/c0v-members/pexels-freestocksorg-64057_y4EMJcpg0.jpg",
+            "tag": "CSS"
+        },
+        {
+            "url": "https://dev.to/babichweb/accordion-tabs-with-pure-css-35ji",
+            "title": "Accordion Tabs with pure CSS",
+            "desc": "This is yet another “article” about how amazing pure CSS is, and how you can do something cool without a single line of JavaScript. Why do I bother to write it? Because I am still fascinated by how amazing pure CSS is.",
+            "imgUrl": "https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/cdn/1_fX1vRVMR8F7jiHGzPyRv7Q_lY8mSsVMg.png",
+            "tag": "CSS"
+        },
+        {
+            "url": "https://medium.com/mirum-budapest/back-to-the-basics-accordions-5aab4a678b3b",
+            "title": "Back to the Basics: Accordions",
+            "desc": "Creating accordions from scratch. Accordions can be very helpful when a page has a lot of content. They let the user scan through the headings and effectively open their content without redirecting them to a new page.",
+            "imgUrl": "https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/cdn/1_fX1vRVMR8F7jiHGzPyRv7Q_lY8mSsVMg.png",
+            "tag": "Javascript"
+        }
+];
